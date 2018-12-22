b0eb7f5c56eedffc148e260a6a88e3ec450e361f
unknown <anderson.parraluiz@gmail.com>
2018-12-21 22:06:59 -0200

    goodfood

---
+++ Good eat.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bem vindo ao Good Food</title>
+ <!-- Latest compiled and minified CSS -->
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="estilo.css">
+
+<!--menu Nav-->
+
+<nav class="navbar navbar-expand-lg navbar-promary bg-warning">
+  <a class="navbar-brand" href="#">O Good Eat</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active navbar-danger" href="#">O Restaurante <span class="sr-only"></span></a>
+      <a class="nav-item nav-link active" href="#">Cardápio</a>
+      <a class="nav-item nav-link active" href="#">Galeria</a>
+      <a class="nav-item nav-link active" href="#">Eventos</a>
+      <a class="nav-item nav-link" href="">Contate-nos</a>
+    </div>
+  </div>
+</nav>
+
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="slide1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item ">
+      <img class="d-block w-100" src="pizza2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="lanche.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Prox</span>
+  </a>
+</div>
+</head>
+<body>
+
+<div id="goodeat">
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+ 
+   <p>Atualmente as pessoas não têm tempo para cozinhar em , não agüentam mais comida congelada, ou só deu para ir almoçar às 15:00 horas e o restaurante fechou entre o almoço e jantar, ou simplesmente querem boa comida e distração... 
+     Agora podem resolver tudo isso de uma vez, no Good Eat Restaurante. Quem conhece as particularidades da comida típica brasileira, 
+      fã de carteirinha. O estabelecimento que nasceu há quase uma década vem se firmando cada vez mais na região Alto Tietê, 
+      já sendo referência até mesmo para as pessoas da capital, sendo freqüentado por empresários, famílias,
+       grupo de amigos, etc... Hoje o estabelecimento já possui uma grande carteira de eventos realizados, desde jantares,
+        eventos temáticos, confraternizações, palestras, cofee breaks, etc... 
+        <br>
+
+
+<img src="imagerestaurante2.jpg" class="rounded-circle" style="width: 500px; height:600; text-align: center"><br>
+<caption><i>foto ilustrativa</i></caption><br>
+<div id="titulo"></div>
+<div id="delivery">
+ <img src="delivery.jpg" class="rounded" style="width: 150px; height:150"></div>
+<legend><h6>Coma o melhor no <I>GOODS EAT<I> em qualquer lugar!<br>
+Peça nossos pratos no conforto de nossa estadia  e saboreie nosso delicioso cardápio <br>Faça sua reserva: (17)29658521 - (17)59856932
+<br>
+Nossos horarios:<br>
+• Expediente:<br>
+CAFÉ DA MANHÃ
+Todos os dias das 7:00 às 11:00 horas
+
+REFEIÇÕES:
+- Segundas e Terças das 11:30 às 15:30 horas<br>
+- Quarta à Sábado das 11:30 às 22:00 horas<br>
+- Aos Domingos das 11:30 às 17:00 horas<br><h6></legend>
+<br>
+<!--boao solicitar-->
+<button class="btn btn-outline-warning" type="button">Solicitar pedido</button><br>
+
+<!--Titulo-->
+<br>
+
+• Restaurante a La carte, refeição preparada sempre no ato do pedido.
+• Cardápio variado, atendendo praticamente todos os gostos.
+• O estabelecimento possui amplo estacionamento privado, dessa forma proporcionando tranquilidade e segurança aos clientes.
+• Contamos com um play ground para as crianças se divertirem.
+• Ambiente rústico e aconchegante
+• Terreno com 10.350 Mts2 arborizado, com árvores frutíferas e nativas, lago com água da sua própria nascente, diversas flores e orquídeas, mantendo você em contato com a natureza.
+• Acessibilidade, banheiro e rampa de acesso para portadores de necessidades especiais.
+• Aceita cartões de crédito, débito, refeição (nacionais e internacionais).
+• Música ao vivo nas sextas-feiras (jantar), aos sábados e aos domingos.
+• Quiosques individuais ao ar livre. <br>
+<br>
+
+<script src="jquery-3.3.1.min.js"></script>
+<script>
+$("a").attr("title", "Deliciosos sorvetes");
+$ ("a").attr("target", "_blank");
+})
+</script>
+
+
+  <div id="titulo">
+<div class="col-md-12 col-sm-6">
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <a href="sorvete2.jpg"><img src="sorvete2.jpg"   style="width: 900px; height:1100" class="img-thumbnail" class="d-block w-90" >
+    </div>
+    <div class="carousel-item">
+      <a href="esfiha2.jpg" ><img src="esfiha2.jpg"   style="width: 900px; height:1100" class="img-thumbnail" class="d-block w-90">
+    </div>
+    <div class="carousel-item">
+    <a href="batata.jpg" ><img src="batata.jpg"   style="width: 900px; height:1100" class="img-thumbnail" class="d-block w-90">
+    </div>
+      <div class="carousel-item">
+      <a href="burger2.jpg" ><img src="burger2.jpg"   style="width: 900px; height:1100" class="img-thumbnail" class="d-block 90"></a>
+    </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Próximo</span>
+  </a>
+</div>
+</div>
+  
+
+
+  <div class="col-md-12">
+<div class="card text-center">
+  <div class="card-header">
+  <div id="titulo">
+
+  
+   
+    <a href=""><img src="cardapio.JPG"  style="width: 300px; height:300"><br>
+    <a href="churrasco.jpg"> <img src="churrasco.jpg" class="img-thumbnail" style= "background-color: whitesmoke ; width: 400px; height:350px"></a>
+<a href="prato3.jpg"><img src="prato3.jpg"  class="img-thumbnail" style= "background-color: whitesmoke ; width: 400px; height: 350px; text-align: center"></a><br>
+<a href="pratosdodia.jpg"><img src="pratosdodia.jpg" class="img-thumbnail" style= "background-color: whitesmoke ; width: 400px; height:350px"></a>
+<a href="maxresdefault.jpg"><img src="maxresdefault.jpg" class="img-thumbnail" style= "background-color: whitesmoke ; width: 400px; height: 350px; text-align: center"></a>
+<br>Participe do nosso evento! <br>Reserve sua mesa antes que acabe.<br>
+<br><a href="#" class="btn btn-outline-warning">Reservar mesa</a>
+</div>
+</div>
+
+<br>
+<h2>Conheça o local</h2>
+<h5>Temos churrasco e musica ao vivo todos os finais de semana</h5>
+<img src="aovivo.jpg"   class="img-thumbnail">
+
+
+<h4>Galeria de fotos</h4>
+
+ <div id="demo" class="carousel slide" data-ride="carousel">
+
+<!-- Indicators -->
+<ul class="carousel-indicators">
+  <li data-target="#demo" data-slide-to="0" class="active"></li>
+  <li data-target="#demo" data-slide-to="1"></li>
+  <li data-target="#demo" data-slide-to="2"></li>
+</ul>
+
+<!-- The slideshow -->
+<div class="carousel-inner">
+  <div class="carousel-item active">
+   <a href="local.jpg"><img src="local.jpg"  style="width: 400px; height:400"></a>
+  </div>
+  <div class="carousel-item">
+   <a href="índice.jpg"> <img src="índice.jpg" style="width: 400px; height:400"></a>
+  </div>
+  <div class="carousel-item">
+   <a href="índice2.jpg"> <img src="índice2.jpg" style="width: 400px; height:300"></a>
+  </div>
+</div>
+
+<!-- Left and right controls -->
+<a class="carousel-control-prev" href="#demo" data-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</a>
+<a class="carousel-control-next" href="#demo" data-slide="next">
+  <span class="carousel-control-next-icon"></span>
+</a>
+
+</div>
+
+<!--comentarios-->
+
+<div class="media border p-2">
+  <div class="media-body">
+  <div id="titulo">
+  <h4>Comentários</h4></div>
+  <img src="avatar.jpg" class="mr-3 mt-3 rounded-circle" style="width:50px;">
+    <h6>Osmar Pina: <small><i><br>Postado em setembro 19, 2018</i></small></h6>
+    <p>ótima comida!, ambiente descontraido e boa musica!</p> 
+    20 dias atrás...<br>
+     <img src="avatar.jpg" class="mr-3 mt-3 rounded-circle" style="width:50px;">
+
+  <div class="media-body">
+  
+    <h6>Ricardo Vanutti: <small><i><br>Postado em agosto 25, 2018</i></small></h6>
+    <p>ótimo atendimento, ambiente descontraido e muito bom</p> 
+    45 dias atrás...
+ 
+<br>
+</div>
+</div>
+</div>
+
+
+
+<div id="form-group">
+
+<h4>Entrega rápida à domicilio</h4>
+
+<a href=""><img src= "entrega1.jpg"  class="rounded-circle"  style="width: 100px; height:100px"></a>
+<div class="cartoes">
+<h5>Aceitamos os cartões</h5>
+<a href=""><img src="master.jpeg" style="width: 40px; height:40px"></a>
+<a href=""><img src="visa.jpg" style="width: 40px; height:40px">
+<a href=""><img src="hiper.png" style="width: 40px; height:30px">
+<a href=""><img src="american2.jpeg" style="width: 40px; height:40px">
+<a href=""><img src="sodexo.jpg" style="width: 40px; height:40px">
+<a href=""><img src="ticket.jpg"  style="width: 40px; height:40px"> 
+</div>
+  </div>
+  </div>
+<!--AKI COMEÇA OS BOTOES REDES SOCIAIS-->
+
+<div id="twitter">
+    <a class="btn btn-block btn-social btn-twitter">
+        <a target="_blank" href= https://twitter.com>
+   <img src="twitter.JPEG" width="40" height="40">
+ 
+  <span class="fa fa-twitter">Siga nos no Twitter</span></a>
+</div>
+<div id="facebook">
+<a class="btn btn-block btn-social btn-facebook">
+    <a target="_blank" href=https://www.facebook.com>
+    <img src="curtanoface.JPEG " width="30" height="30">
+  <span class="facebook.com.br">Curta no facebook</span></a>
+</div>
+
+<div id="instagran">
+    <a class="btn btn-block btn-social btn-instagran">
+        <a target="_blank" href=https://www.instagram.com>
+       
+      <img src="instagramiconenovo.jpg" width="30" height="30"> 
+    <span class="fa fa-instagran"> Siga nos no Instagran</span>
+    </a>
+</div>
+
+<form method="get" action="dados.php">
+
+<footer>
+
+<article>
+
+<div id="form-group">
+<div class="form-group">
+<br>Fale Conosco : <br>faleconosco@faleconosco.com<br>
+    <label for="exampleFormControlTextarea1"><h3>Duvidas / Sugestões</h3></label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"></textarea>
+
+  </div>
+</form>
+<div id="form">
+<form class="form-inline" action="/action_page.php">
+<label for="text" class="mr-sm-3">Nome Completo</label><br>
+  <input type="text" class="form-control mb-2 mr-sm-2" id="nome">
+  <label for="email" class="mr-sm-2">Email:</label><br>
+  <input type="email" class="form-control mb-2 mr-sm-2" id="email">
+ 
+  <div class="form-check mb-2 mr-sm-2">
+    <label class="form-check-label">
+      <input class="form-check-input" type="checkbox"> lembrar me
+    </label>
+  </div>
+  <button type="submit" class="btn btn mb-2">Enviar</button>
+</form> 
+</div>
+</div>
+
+<br>
+<!--AKI COMEÇA O RODAPÉ-->
+<div id="footer">
+        <footer><strong>Sobre este site:<br>
+          Estamos empenhados em garantir que este site seja acessível a todos. Se você tiver dúvidas ou sugestões sobre a acessibilidade deste site,<br> entre em contato conosco.<br></strong>
+          </footer>
+            <div id="article">
+              <a target="_blank" href=https://setup-web.webnode.com style="color: white">
+                <br>2018-Desenvolvido por Web Setup®</strong></a>
+              </article><article> 
+            </div>
+
+</body>
+
+
+
+
+</html>
+
